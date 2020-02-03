4d3d2b772d2618fa58b295ef17fdff00876d8f32
Vlast92 <vlasteg@mail.ru>
2020-02-03 16:54:28 +0300

    Version 1.3.11 -small fix in the popup template

--- tmpl/popup.php
+++ tmpl/popup.php
@@ -21,58 +21,60 @@ if($debug)
 $form_id = "seoclick_form_". $module->id . "_" . rand(1, 10000);
 ?>
     <div class="seoclickFormsShowpopUpForm <?=$showButtonCss?>" data-form="<?= $form_id ?>"><?= $showButtonText ?></div>
-    <div id="<?= $form_id ?>" class="seoclick-forms seoclick-forms-popup-wrap <?= $moduleclass_sfx; ?>">
-        <form class="form-validate" data-moduleid="<?= $form_id ?>">
-            <div class="container">
-				<?php if ($formTitle): ?>
-					<?php require JModuleHelper::getLayoutPath('mod_seoclick_forms', 'form_title');?>
-				<?php endif; ?>
-	            <?php if ($formText): ?>
-		            <?php require JModuleHelper::getLayoutPath('mod_seoclick_forms', 'form_text');?>
-	            <?php endif; ?>
-                <div class="message-container"></div>
-				<?php foreach ($formFields as $name => $formField): ?>
-                    <div class="field-wrap  <?= $formField['css'] ?>">
-						<?php if ($formField['label']): ?>
-                        <label>
+    <div style="display: none;">
+        <div id="<?= $form_id ?>" class="seoclick-forms seoclick-forms-popup-wrap <?= $moduleclass_sfx; ?>">
+            <form class="form-validate" data-moduleid="<?= $form_id ?>">
+                <div class="container">
+				    <?php if ($formTitle): ?>
+					    <?php require JModuleHelper::getLayoutPath('mod_seoclick_forms', 'form_title');?>
+				    <?php endif; ?>
+				    <?php if ($formText): ?>
+					    <?php require JModuleHelper::getLayoutPath('mod_seoclick_forms', 'form_text');?>
+				    <?php endif; ?>
+                    <div class="message-container"></div>
+				    <?php foreach ($formFields as $name => $formField): ?>
+                        <div class="field-wrap  <?= $formField['css'] ?>">
+						    <?php if ($formField['label']): ?>
+                            <label>
                             <span class="fieldName">
                                 <?= $formField['label']; ?>
 	                            <?php if ($formField['required']): ?><span class="required">*</span><?php endif;?>
                             </span>
-							<?php endif; ?>
-	                        <?php switch ($formField['type'])
-	                        {
-		                        case "line_text":
-			                        break;
-		                        case "textarea":
-			                        require JModuleHelper::getLayoutPath('mod_seoclick_forms', 'field_textarea');
-			                        break;
-		                        case "select":
-			                        require JModuleHelper::getLayoutPath('mod_seoclick_forms', 'field_select');
-			                        break;
-		                        case "file":
-			                        require JModuleHelper::getLayoutPath('mod_seoclick_forms', 'field_file');
-			                        break;
-		                        case "date":
-			                        require JModuleHelper::getLayoutPath('mod_seoclick_forms', 'field_date');
-			                        break;
-		                        default:
-			                        require JModuleHelper::getLayoutPath('mod_seoclick_forms', 'field_default');
-	                        } ?>
-							<?php if ($formField['label']): ?>
-                        </label>
-					<?php endif; ?>
-                    </div>
-				<?php endforeach; ?>
-	            <?php
-	            if ($joomlaRecapchaEnabled)
-	            {
-		            require JModuleHelper::getLayoutPath('mod_seoclick_forms', 'field_recaptcha');
-	            } ?>
-                <input type="hidden" name="module-name" value="<?= htmlspecialchars($module->title) ?>"/>
-                <div class="field-wrap submit-button-wrap"><input type="submit" class="<?=$submitCss?>" value="<?= $submitText; ?>"/></div>
-            </div>
-        </form>
+							    <?php endif; ?>
+							    <?php switch ($formField['type'])
+							    {
+								    case "line_text":
+									    break;
+								    case "textarea":
+									    require JModuleHelper::getLayoutPath('mod_seoclick_forms', 'field_textarea');
+									    break;
+								    case "select":
+									    require JModuleHelper::getLayoutPath('mod_seoclick_forms', 'field_select');
+									    break;
+								    case "file":
+									    require JModuleHelper::getLayoutPath('mod_seoclick_forms', 'field_file');
+									    break;
+								    case "date":
+									    require JModuleHelper::getLayoutPath('mod_seoclick_forms', 'field_date');
+									    break;
+								    default:
+									    require JModuleHelper::getLayoutPath('mod_seoclick_forms', 'field_default');
+							    } ?>
+							    <?php if ($formField['label']): ?>
+                            </label>
+					    <?php endif; ?>
+                        </div>
+				    <?php endforeach; ?>
+				    <?php
+				    if ($joomlaRecapchaEnabled)
+				    {
+					    require JModuleHelper::getLayoutPath('mod_seoclick_forms', 'field_recaptcha');
+				    } ?>
+                    <input type="hidden" name="module-name" value="<?= htmlspecialchars($module->title) ?>"/>
+                    <div class="field-wrap submit-button-wrap"><input type="submit" class="<?=$submitCss?>" value="<?= $submitText; ?>"/></div>
+                </div>
+            </form>
+        </div>
     </div>
 <?php
 require JModuleHelper::getLayoutPath('mod_seoclick_forms', 'form_params');
