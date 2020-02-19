c163cb8c0657266886e9682bdd9ec7d330610b0d
Vlast92 <vlasteg@mail.ru>
2020-02-19 11:58:40 +0300

    Version 1.3.13 -added placeholder for a select input type; -updated styles for a select input type.

--- tmpl/field_select.php
+++ tmpl/field_select.php
@@ -10,8 +10,13 @@
 defined('_JEXEC') or die;
 ?>
 <select name="<?= $name; ?>">
-    <option selected
-            disabled><?= jText::_("MOD_SEOCLICK_FORMS_SELECT_CHOOSE_OPTION") ?></option>
+    <option selected disabled>
+        <?php if($formField['placeholder']): ?>
+            <?=$formField['placeholder']?>
+        <?php else: ?>
+	        <?= jText::_("MOD_SEOCLICK_FORMS_SELECT_CHOOSE_OPTION") ?>
+        <?php endif; ?>
+    </option>
 	<?php $options = explode("\r\n", $formField['select_options']);
 	foreach ($options as $option):?>
         <option value="<?= $option ?>"><?= $option ?></option>
